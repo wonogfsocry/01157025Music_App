77386a8902ee9061217fa02d62905bd335c15646
wonogfsocry <yangneil930406@gmail.com>
2025-01-03 15:21:12 +0800

    Initial Commit

---
+++ 01157025Music_App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  01157025Music_App
+//
+//  Created by user10 on 2025/1/3.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 01157025Music_App/_1157025Music_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  _1157025Music_AppApp.swift
+//  01157025Music_App
+//
+//  Created by user10 on 2025/1/3.
+//
+
+import SwiftUI
+
+@main
+struct _1157025Music_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
